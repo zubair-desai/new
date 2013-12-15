c14e456e9623f3f04862d5854a5fc39da2db746c
zubair-desai <zubair.desai@gmail.com>
2013-12-15 13:26:24 -0800

    Finish user signup and styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,12 @@
 SampleApp::Application.routes.draw do
  
-  get "users/new"
+  resources :users
   root 'static_pages#home'
 
-  #get "static_pages/home"
-  #match "/help", to: "statci_pages#help", via: "get"
-
-  #get "static_pages/help"
+  # => ex. get "static_pages/help"
   match "/help", to: "static_pages#help", via: "get"
-
-  #get "static_pages/about"
   match "/about", to: "static_pages#about", via: "get"
-
-  #get "static_pages/contact"
   match "/contact", to: "static_pages#contact", via: "get"
-
   match "/signup", to: "users#new", via: "get"
 
 
